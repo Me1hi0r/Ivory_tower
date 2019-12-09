b3eba13296ada5cdd6403e3524c95bdb332822e8
Grisha <grilitovchenko@gmail.com>
2019-12-09 23:35:22 +0300

    Add keyboard

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Ivory_tower
+
+heroku create bot-test --buildpack heroku/python --region eu
+
+heroku buildpacks:set heroku/python
+
+git push heroku master
+heroku ps:scale bot=1
+heroku logs --tail
+heroku ps:stop bot
--- bot.py
+++ bot.py
@@ -1,15 +1,24 @@
 import telebot
+from config import TOKEN
 from telebot.types import Message
+from telebot import types
 
-TOKEN = '988812278:AAGRpqMrPRHZGYYtFnDnySmVcAcpmJAHIBo'
+
 bot = telebot.TeleBot(TOKEN)
+
 
 @bot.message_handler(func=lambda message: True)
 def upper(message: Message):
     bot.reply_to(message, message.text.upper())
 
-bot.polling()
 
-#fgfhtyt
-#python bot.py
+@bot.message_handler(commands=['start'])
+def process_start_command(message: Message):
+    markup = types.ReplyKeyboardMarkup()
+    markup.row('☉', '☽', '☿', '♀', '♂')
+    markup.row('♃', '♄', '♅', '♆', '♇')
+    bot.send_message(message.chat.id, "Choose one letter:", reply_markup=markup)
+
+
+bot.polling()
 

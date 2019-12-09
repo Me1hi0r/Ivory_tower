d99acc9c34e29c733228a42f1ca6c3a298951824
Grisha <grilitovchenko@gmail.com>
2019-12-09 23:39:51 +0300

    Add keyboard

--- bot.py
+++ bot.py
@@ -7,17 +7,20 @@ from telebot import types
 bot = telebot.TeleBot(TOKEN)
 
 
-@bot.message_handler(func=lambda message: True)
-def upper(message: Message):
-    bot.reply_to(message, message.text.upper())
-
-
 @bot.message_handler(commands=['start'])
 def process_start_command(message: Message):
     markup = types.ReplyKeyboardMarkup()
     markup.row('☉', '☽', '☿', '♀', '♂')
     markup.row('♃', '♄', '♅', '♆', '♇')
     bot.send_message(message.chat.id, "Choose one letter:", reply_markup=markup)
+
+
+@bot.message_handler(func=lambda message: True)
+def upper(message: Message):
+    bot.reply_to(message, message.text.upper())
+
+
+
 
 
 bot.polling()
